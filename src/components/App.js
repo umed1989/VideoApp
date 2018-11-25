c1cbfaedecd6ae951dd0de073ff2763137a5001a
umed tolibi <tolibiumed@gmail.com>
2018-11-24 20:07:00 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../APIS/Youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui-container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = {
+    term: ""
+  };
+
+  onInputChange = e => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+
+    this.props.onFormSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui-segment search-bar">
+        <form className="ui-form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label> Video Search</label>
+            <input
+              value={this.state.term}
+              type="text"
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
